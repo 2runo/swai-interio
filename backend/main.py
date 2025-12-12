c61aeb37aa7cdf4ece141a942b36d2b43a4158d8
조준희 <61267275+2runo@users.noreply.github.com>
2025-12-12 11:17:18 +0900

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,7 @@ allowed_origins = (
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins,
+    allow_origins=['*'],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
